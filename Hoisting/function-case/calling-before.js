bbdf45611bf5fdf5b174bf99db0add497f9725a3
Vivek Kumar <vivekkr0311@gmail.com>
2023-12-22 19:38:23 +0530

    Hoisting: Explaination of hoisting of arrow function and normal function in a js

--- Hoisting/function-case/calling-before.js
+++ Hoisting/function-case/calling-before.js
@@ -1,5 +1,11 @@
 console.log(getName);
 
+//Only in this case entire copy of the function body is being copy 
+//in the first to a variable 'getName'.
+//this is why javascript knows 'getName' is a function and has function body.
+
+//In the case of arrow function this does not happen, as arrow function are stored in a variable.
+//and variable in javascript during first phase has value assinged 'undefined'.
 function getName(){
     console.log("Vivek");
 }
