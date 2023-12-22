31c7226b51402ba2075847ff1942f28146a5ae79
Vivek Kumar <vivekkr0311@gmail.com>
2023-12-22 19:09:34 +0530

    Hoisting: Learnig hoisting

---
+++ Hoisting/learn-hoisting.js
@@ -0,0 +1,11 @@
+getName();
+console.log(x); //Variable is there but with value 'undefined'
+
+// console.log(y); 
+//This will try to fin=d variable 'y', but it is not there
+// hence 'variable not defined error will come'
+
+var x = 10;
+function getName(){
+    console.log("Vivek");
+}
