62d9a3bd20eabe1015c338393ee4454d3b00be6a
Vivek Kumar <vivekkr0311@gmail.com>
2023-12-22 19:31:57 +0530

    Hoisting: Hoisting with arrow functions

---
+++ Hoisting/arrow-function-hoisting/arrow-function-hoisting-two.js
@@ -0,0 +1,5 @@
+var getName = () => {
+    console.log("Vivek");
+}
+//It will not show error, as here javascript will know 'getName' is a function.
+getName();
---
+++ Hoisting/arrow-function-hoisting/arrow-function-hoisting.js
@@ -0,0 +1,5 @@
+getName();
+//It will show an error, as javascript will not know that 'getName' is a function.
+var getName = () =>{
+    console.log("Vivek");
+}
