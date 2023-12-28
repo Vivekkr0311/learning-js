88450cbb8a19738675e66a092734c688dd681040
Vivek Kumar <vivekkr0311@gmail.com>
2023-12-28 13:35:34 +0530

    Notes add for  setTimeout question solution

--- setTimouts_and_closures/printingNumberSeq_solution.js
+++ setTimouts_and_closures/printingNumberSeq_solution.js
@@ -7,3 +7,11 @@ function x(){
     }
 }
 x();
+
+//'let' has block scope.
+//Each time this loop runs, there is new variable 'i'.
+//It is new copy of 'i' all together.
+
+//So at last 5 copies of 'i' is created each having a value 1, 2, 3, 4, and 5.
+//And in each call, setTimeout puts the function in separate location with new 'i (1, 2, 3, 4 and 5) * 1000', and function will
+//take new copy of 'i' in its 'Closure'.
