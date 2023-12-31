2058df982f873f434e4bac199751d213d5bba01e
Vivek Kumar <vivekkr0311@gmail.com>
2023-12-31 12:25:13 +0530

    Functions: Function declarations

---
+++ Function-Working-In-JS/function-declaration.js
@@ -0,0 +1,9 @@
+//What is a function declaration ??
+
+//Function statement is also known as Function declaration.
+//If you say function statement or function declaration, both are the same thing.
+
+//This is a function statement or a function declaration.
+function a(){
+    console.log("a is called");
+}
