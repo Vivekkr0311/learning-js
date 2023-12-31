3d3b41575031e9f6acb2f2c3a504879b30f5339b
Vivek Kumar <vivekkr0311@gmail.com>
2023-12-31 12:22:01 +0530

    Functions: Explaination difference between function statement and function expression

--- Function-Working-In-JS/function-expression.js
+++ Function-Working-In-JS/function-expression.js
@@ -2,5 +2,7 @@
 //We can assign function to a variable, function acts as a value.
 
 var b = function(){
-    
+
 }
+
+//Function statement and Function expression both are ways of creating a function in javascript.
