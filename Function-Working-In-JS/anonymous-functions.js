b3e1ff167005b1d94c21c122d318be9206bfceb0
Vivek Kumar <vivekkr0311@gmail.com>
2023-12-31 12:34:15 +0530

    Functions: Anonymous functions

---
+++ Function-Working-In-JS/anonymous-functions.js
@@ -0,0 +1,22 @@
+//What is a anonymous function ??
+//Anonymous function is function is a function wihtout a name.
+//Anonymous functions do not have their own identity.
+
+//Like:
+//This will give a syntax error. Because while creating functions using statement requires a name.
+
+function (){
+
+}
+
+//So, if it give a syntax error what is use of anonymous function.
+
+//Anonymous function are use in the places where we need function as value.
+//So, anonymous function are used to assign a function in a value.
+//Like:
+var b =  function(){
+
+}
+
+//In this function expression we used anonymous function to assign to a variable.
+//But we cannot use anonymous function with function statements.
