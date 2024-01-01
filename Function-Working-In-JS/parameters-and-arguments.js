272967f3d53248cb37e32ba949f7a74484ef5e0a
Vivek Kumar <vivekkr0311@gmail.com>
2024-01-01 13:22:29 +0530

    Function: Difference between Arguments and Parameters

--- Function-Working-In-JS/parameters-and-arguments.js
+++ Function-Working-In-JS/parameters-and-arguments.js
@@ -3,3 +3,6 @@ function add(param1, param2){
 }
 var res = add(1, 2 /*Arguments8*/);
 console.log(res);
+
+//The values which are passed inside the function are called "Arguments".
+//The labels in function which gets those arguments are called "Parameters".
