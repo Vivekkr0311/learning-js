8eb82ef4cf5053821632d20bc9bc31ab368aa82e
Vivek Kumar <vivekkr0311@gmail.com>
2024-01-01 13:33:15 +0530

    Functions: First class functions

---
+++ Function-Working-In-JS/first-class-function.js
@@ -0,0 +1,19 @@
+//The ability to use function as value, is called "First class function".
+// - functions can be passed as arguments in another function.
+// - functions can returned by a function as value.
+// - functions can be stored in a variable as value.
+// This ability is known as "First class functions".
+
+function a(){
+    console.log("a normal function.")
+}
+
+function b(param){
+    console.log(param);
+    return function () {
+        console.log("Function returned.")
+    }
+}
+
+var z = b(a);
+z();
