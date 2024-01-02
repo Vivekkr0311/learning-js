cf0fb9fe4e614ad4ebbd62abcadee05a6dac93e3
Vivek Kumar <vivekkr0311@gmail.com>
2024-01-02 14:14:33 +0530

    Functions: Main thread blocking explaination

--- Function-Working-In-JS/advantage-callback.js
+++ Function-Working-In-JS/advantage-callback.js
@@ -2,6 +2,13 @@
 //callback function and called sometime later in the program, this time is second paramet into the
 //setTimeout() function.
 
+//Javascript have only one "Callstack", that is called main thread of javascript. Everything in javascript is executed
+//through this callstack, if anything blocking this callstack that is called blocking the main thread of javascript.
+
+
+//This is why, we should use Async things in javascript for the things which takes so much time.
+//like setTimeout.
+
 setTimeout(function (){
     console.log("timer");
 }, 5000);
@@ -19,7 +26,7 @@ x(function y(){
 //a time in order.
 //So first,
 // 1. Javascript will not wait for 5 secs here, it will
-//    execute the next line of code.
+//    execute the next line of code. setTimeout will put this input function in callstack after 5 seconds.
 //    setTimeout will take the function and put it somewhere else, and will wait for 5000 milisec (5 seconds)
 //    then it will take it back, and it will execute this function. 
 
