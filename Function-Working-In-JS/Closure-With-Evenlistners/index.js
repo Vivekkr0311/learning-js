642873649ea988677b79b514393e9dc508e311fc
Vivek Kumar <vivekkr0311@gmail.com>
2024-01-03 11:43:19 +0530

    Functions: Explaination of Closure with callback function, Handler

--- Function-Working-In-JS/Closure-With-Evenlistners/index.js
+++ Function-Working-In-JS/Closure-With-Evenlistners/index.js
@@ -24,3 +24,7 @@ attachEventListner();
 
 //Now the callback function "xyz" will form a Closure with the variable "count".
 //And this call function kind of remembers where this "count" is present.
+
+//Every event has handle with it, in this case we have this callback function as a event handler.
+//And this event handler (our call function "xyz"), have the scope chain, it has closure.
+//It knows lexical environment of its parent, and global.
