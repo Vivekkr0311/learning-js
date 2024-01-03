edf2eb27cd2aade234265373f2924e1583327d37
Vivek Kumar <vivekkr0311@gmail.com>
2024-01-03 10:53:28 +0530

    Functions: Closure with event listners file add

---
+++ Function-Working-In-JS/Closure-With-Evenlistners/index.js
@@ -0,0 +1,6 @@
+//This is use case of callback functions.
+document.addEventListener("DOMContentLoaded", function() {
+    document.getElementById("clickMe").addEventListener("click", function(){
+        alert("Button clicked!");
+    });
+});
