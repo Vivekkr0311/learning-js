88699cc29b95f511f5da7412c481d13d148b2d2e
Vivek Kumar <vivekkr0311@gmail.com>
2024-01-03 11:37:47 +0530

    Functions: Closure implementation with event listeners

--- Function-Working-In-JS/Closure-With-Evenlistners/index.js
+++ Function-Working-In-JS/Closure-With-Evenlistners/index.js
@@ -1,9 +1,26 @@
 //This is use case of callback functions.
 //This is not good practice using global variable.
-let count = 0;
-document.addEventListener("DOMContentLoaded", function() {
-    document.getElementById("clickMe").addEventListener("click", function(){
-        ++count;
-        alert("Button clicked! " + count);
+// let count = 0;
+// document.addEventListener("DOMContentLoaded", function() {
+//     document.getElementById("clickMe").addEventListener("click", function(){
+//         ++count;
+//         alert("Button clicked! " + count);
+//     });
+// });
+
+//So we will make a closure, inside which this we will put this global variable, and in this way
+//we will make use of data hidding.
+
+function attachEventListner(){
+    let count = 0;
+    document.addEventListener("DOMContentLoaded", function xyz() {
+        document.getElementById("clickMe").addEventListener("click", function(){
+            ++count;
+            alert("Button clicked! " + count);
+        });
     });
-});
+}
+attachEventListner();
+
+//Now the callback function "xyz" will form a Closure with the variable "count".
+//And this call function kind of remembers where this "count" is present.
