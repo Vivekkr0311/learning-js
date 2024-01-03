4a319585bc899ccfa728408415d7cc257bdb3947
Vivek Kumar <vivekkr0311@gmail.com>
2024-01-03 11:32:58 +0530

    Functions: Issue with global variable

--- Function-Working-In-JS/Closure-With-Evenlistners/index.js
+++ Function-Working-In-JS/Closure-With-Evenlistners/index.js
@@ -1,6 +1,9 @@
 //This is use case of callback functions.
+//This is not good practice using global variable.
+let count = 0;
 document.addEventListener("DOMContentLoaded", function() {
     document.getElementById("clickMe").addEventListener("click", function(){
-        alert("Button clicked!");
+        ++count;
+        alert("Button clicked! " + count);
     });
 });
