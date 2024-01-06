cb084820598a379b9ac0b7ad0911a775287f18bc
Vivek Kumar <vivekkr0311@gmail.com>
2024-01-06 13:16:57 +0530

    Trust Issues setTimeout: Blocking the main thread

--- Trust_Issues_With_setTimeouts/01_Example.js
+++ Trust_Issues_With_setTimeouts/01_Example.js
@@ -4,3 +4,5 @@ function cb(){
 setTimeout(cb, 5000);
 
 //setTimeout not waits for exactly 5 seconds.
+//setTimeout does not gurantee that callback will be executed
+//after 5 seconds, it all depends on callstack.
