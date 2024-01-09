5ce47eaa6279b554575957a7d75c231dd236a0c7
Vivek Kumar <vivekkr0311@gmail.com>
2024-01-09 19:05:26 +0530

    Higher Order Function: File add

---
+++ Higher_Order_Function/01_Example_Higher_Order_Function.js
@@ -0,0 +1,10 @@
+function x(){
+    console.log("Namaste.");
+}
+
+function y(x){
+    x();
+}
+
+//Here, "x" will be called as callback function and
+//and "y" will be called as Higher Order function.
