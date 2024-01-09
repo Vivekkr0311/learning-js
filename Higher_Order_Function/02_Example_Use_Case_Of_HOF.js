d176a0e4c8c7c4449a81db1e45e1c62aa0a5d217
Vivek Kumar <vivekkr0311@gmail.com>
2024-01-09 19:13:36 +0530

    Higher Order Function: Naive example add

---
+++ Higher_Order_Function/02_Example_Use_Case_Of_HOF.js
@@ -0,0 +1,12 @@
+//Naive way of calculating area of a circle using the list of radius.
+
+const radius = [3, 1, 2, 4]
+
+const calculateArea = function (radius){
+    const output = [];
+    for(let i = 0; i < radius.length; i++){
+        output.push(Math.PI * radius[i] * radius[i]);
+    }
+    return output;
+};
+console.log(calculateArea(radius));
