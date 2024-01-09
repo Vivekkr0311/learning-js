550d2ba418675ee3952f448a925c0cbb826d22b1
Vivek Kumar <vivekkr0311@gmail.com>
2024-01-09 18:46:44 +0530

    Trust Issues: setTimeout with 0

--- Trust_Issues_With_setTimeouts/03_Example_setTimeout_with_0.js
+++ Trust_Issues_With_setTimeouts/03_Example_setTimeout_with_0.js
@@ -8,3 +8,6 @@ console.log("End");
 
 //It will still print Start first then End and then Callback.
 //Because callback will still get into the callback queue then it will be executed.
+
+//Sometimes we want to execute some code first and other code later
+//in such cases we use setTimeout with 0 time.
