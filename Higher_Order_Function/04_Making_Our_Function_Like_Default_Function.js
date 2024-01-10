51cd60faf12e8a18bc67dd7b0fce06b63d60bbb0
Vivek Kumar <vivekkr0311@gmail.com>
2024-01-10 10:51:43 +0530

    Higher Order Function: Making function call similar to default function

---
+++ Higher_Order_Function/04_Making_Our_Function_Like_Default_Function.js
@@ -0,0 +1,32 @@
+const radius = [3, 1, 2, 4];
+
+const area = function (r){
+    return 2 *  Math.PI * r * r;
+}
+
+const circumeference = function (r){
+    return 2 *  Math.PI * r;
+}
+
+const diameter = function (r){
+    return 2 * r;
+}
+
+//Now this function "calculate" will be available for array that you will create.
+Array.prototype.calculate = function (logic){
+    const output = [];
+    for(let i = 0; i < radius.length; i++){
+        output.push(logic(radius[i]));
+    }
+    return output;
+}
+
+//Now we can call "calculate" in the same way as we can call "map".
+// const = output = radius.map(area);
+const areas = radius.calculate(area);
+const circumeferenc = radius.calculate(circumeference);
+const diamete = radius.calculate(diameter);
+
+console.log("Area calculated: " + areas);
+console.log("Circumference calculated: " + circumeferenc);
+console.log("Diameter calculated: " + diamete);
