dd8fb3aef1ea694a90d51ab5b65ce97d16b381f7
Vivek Kumar <vivekkr0311@gmail.com>
2024-01-11 13:44:54 +0530

    Built-in HOF: Tricky map use case

---
+++ Built-in-HOF/05_Example_Built-in_HOF_tricky_example.js
@@ -0,0 +1,10 @@
+// Assume these data came from an API.
+const users = [
+    { firstName: "akshay", lastName: "saini", age: 26 },
+    { firstName: "donald", lastName: "trump", age: 75 },
+    { firstName: "elon", lastName: "musk", age: 50 },
+    { firstName: "deepika", lastName: "padukone", age: 26 },
+];
+
+const output = users.map((x) => x.firstName + " " + x.lastName);
+console.log(output);
