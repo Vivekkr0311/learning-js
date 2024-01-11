e5d1dfff75910cb7dfc73dee1894ff114f8a801b
Vivek Kumar <vivekkr0311@gmail.com>
2024-01-11 12:50:15 +0530

    Built-in HOF: reduce use case add

--- Built-in-HOF/03_Example_Concept_Of_Reduce.js
+++ Built-in-HOF/03_Example_Concept_Of_Reduce.js
@@ -11,3 +11,14 @@ function findSum(arr){
 }
 
 console.log(findSum(arr));
+
+const sum = arr.reduce(function(acc, curr){
+    acc = acc + curr;
+    return acc;
+}, 0);
+console.log(sum);
+//reduce takes two parametes.
+// 1. first one is a callback function with two parametes, accumulator and current.
+//    accumulator: this parameter is the refernce to the value accumulated, actually our final result.
+//    current: this is the reference to each value for our array.
+// 2. second parameter is the initial value for our accumalator.
