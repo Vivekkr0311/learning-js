964531382ca35210677bf961df74d5ea58faa959
Vivek Kumar <vivekkr0311@gmail.com>
2024-01-16 13:18:07 +0530

    Promise APIs: Promise.allSettled() example file add

---
+++ 19_Promise_API_Interview_Questions/04_Example_Promise_allSetteled.js
@@ -0,0 +1,26 @@
+const p1 = new Promise((resolve, reject) => {
+    //This is mimicing an API call, which will give success message after 3 secs.
+    setTimeout(() => resolve("P1 success"), 3000);
+});
+
+const p2 = new Promise((resolve, reject) => {
+    //This is mimicing an API call, which will give success message after 1 sec.
+    setTimeout(() => resolve("P2 success"), 1000);
+});
+
+const p3 = new Promise((resolve, reject) => {
+    //This is mimicing an API call, which will be rejected after 2 secs.
+    setTimeout(() => reject("P3 fail"), 2000);
+});
+
+
+//p1, p2, and p3 are promises, these can be API calls also.
+//Promise.allSettled() will wait for all the promise to get setteled, then
+//it will give the result of respective promises.
+Promise.allSettled([p1, p2, p3]).then(res => {
+    console.log(res);
+}).catch((err) => {
+    //Always this type of "catch" clause to handle "uncaught error".
+    //It is not good practice to leave uncaught error.
+    console.error(err); //So, this will be a caught error.
+});
