3f39cfd374e92c9296d51e4899bff99affd82583
Vivek Kumar <vivekkr0311@gmail.com>
2024-01-20 16:04:42 +0530

    Async Await: Multiple promises example

--- 20_Async_Await/09_async_await_example.js
+++ 20_Async_Await/09_async_await_example.js
@@ -1,14 +1,15 @@
 const p1 = new Promise((resolve, reject) => {
     setTimeout(() => {
         resolve("Promise resolved from 1");
-    }, 10000);
+    }, 5000);
 });
 
 const p2 = new Promise((resolve, reject) => {
     setTimeout(() => {
         resolve("Promise reolved from 2")
-    }, 5000);
+    }, 10000);
 });
+//We reversed the wait for above code. Now, p1 will get resolved first.
 
 async function handlePromise(){
     //This line will be executed immediately.
@@ -16,13 +17,18 @@ async function handlePromise(){
 
     //JS Engine will wait for Promise to be resolved. (It does not actually wait, it appears to be waiting)
     //Program will wait here.
+    //This will be printed after 5 secs.
+    //And after executing below this function "handlePromise" will be suspended from the call stack.
+    //so that other things can be executed, as javascript is a synchronous single threaded programming languaage.
     const val = await p1;
     console.log("Namaste Javascript 1");
     console.log(val);
 
     //Calling second time.
+    //This will be printed after 10 secs.
     const val2 = await p2;
     console.log("Namaste Javascript 2");
     console.log(val2);
 }
+
 handlePromise();
