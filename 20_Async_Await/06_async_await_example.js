d656039cf6fe6c43353856d83ae1737506658573
Vivek Kumar <vivekkr0311@gmail.com>
2024-01-20 15:29:54 +0530

    Async Await: Handling Promises using await

---
+++ 20_Async_Await/06_async_await_example.js
@@ -0,0 +1,19 @@
+const p = new Promise((resolve, reject) => {
+    setTimeout(() => {
+        resolve("Promise resolved");
+    }, 10000);
+});
+
+async function handlePromise(){
+    //This line will be executed immediately.
+    console.log("Hello World!");
+
+    //JS Engine will wait for Promise to be resolved.
+    //Program will wait here.
+    const val = await p;
+
+    //Once the promise is resolved then below code is executed.
+    console.log("Namaste Javascript");
+    console.log(val);
+}
+handlePromise();
