b99b0f27161ef47be79bb1e8bb6315d13c5bbaae
Vivek Kumar <vivekkr0311@gmail.com>
2024-01-21 11:58:02 +0530

    this keyword: this in function and strict mode of Javascript

---
+++ 21_this_keyword/02_this_in_function.js
@@ -0,0 +1,8 @@
+//Here, we are writing code in non-strict mode.
+console.log(this);
+
+function x(){
+    console.log(this);
+}
+
+x();
---
+++ 21_this_keyword/03_this_in_strict_mode.js
@@ -0,0 +1,9 @@
+//Here, we are writing code in strict mode.
+"use strict";
+console.log(this);
+
+function x(){
+    console.log(this);
+}
+
+x();
