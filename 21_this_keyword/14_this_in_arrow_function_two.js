16697f1d934ca3a151b8700e62dec36d99f58110
Vivek Kumar <vivekkr0311@gmail.com>
2024-01-23 15:21:19 +0530

    this keyword: this inside DOM example add

--- 21_this_keyword/14_this_in_arrow_function_two.js
+++ 21_this_keyword/14_this_in_arrow_function_two.js
@@ -14,3 +14,6 @@ console.log(this);
 //As the arrow function is enclose inside the another function which is inside
 //a object, so arrow function will point to lexical environment of its parent function
 //where it is printed.
+
+//arrow function does not provide their own "this" binding, it retains the value of "this"
+//from its lexical context or lexical environment.
